8fdefb1c9bc142a7e9bd72e2d19276d92e94999d
Rommel Rico <me@rommelrico.com>
2018-10-30 08:50:41 -0700

    Setting a video view and media controller

--- app/src/main/java/com/rommelrico/androidvideodemo/MainActivity.kt
+++ app/src/main/java/com/rommelrico/androidvideodemo/MainActivity.kt
@@ -2,11 +2,21 @@ package com.rommelrico.androidvideodemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val videoView = findViewById<VideoView>(R.id.videoView)
+        videoView.setVideoPath("android.resource://${packageName}/${R.raw.demovideo}")
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        videoView.setMediaController(mediaController)
+        videoView.start()
+        
     }
 }
